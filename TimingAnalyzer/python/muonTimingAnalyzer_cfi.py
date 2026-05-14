47faeaa8aebee4913e48d0b080e5265fd60f7708
Lucas Nestor <nestor.40@osu.edu>
2026-05-13 20:13:13 -0500

    Remove TimingAnalyzer module as it is unused

--- TimingAnalyzer/python/muonTimingAnalyzer_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-muonTimingAnalyzer = cms.EDAnalyzer("MuonTimingAnalyzer",
-    # NanoAODv9 selection applied in C++: pt > 3 && (standard IDs)
-    muonSrc     = cms.InputTag("slimmedMuons"),
-    # NanoAODv9 selection applied in C++: pt > 5
-    electronSrc = cms.InputTag("slimmedElectrons"),
-)
